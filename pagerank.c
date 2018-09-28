9b243d0bc5f926999572cef0f31a84c6db77696c
Matty boi <z5187551@student.unsw.edu.au>
2018-09-28 14:07:53 +1000

    initial file

---
+++ pagerank.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]){
+
+	if(argc != 4){
+		printf("Wrong number of arguments!\n");
+		abort();
+	}
+	float d = argv[1];
+	float diffPR = argv[2];
+	int maxIteration = argv[3];
+
+	
+
+
+}
